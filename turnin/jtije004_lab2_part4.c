6b2aeb7d66c7638d0c1a402ed18541a64f76cc2c
Jeana Tijerina <jeana2019@gmail.com>
2021-04-11 23:47:38 -0700

    update turnin

--- turnin/jtije004_lab2_part4.c
+++ turnin/jtije004_lab2_part4.c
@@ -8,6 +8,8 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <stdlib.h>
+#include <stdio.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
@@ -20,39 +22,41 @@ int main(void) {
 	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
 //    unsigned char tmpB = 0x00;
-    unsigned char tmpA; //= PINA & 0X01;
-    unsigned char tmpB; // = PINA & 0x02;
-    unsigned char tmpC;
-    unsigned char tmpD;
-    unsigned char totalWeight;
-    unsigned char difference;
+    unsigned char tmpA = 0x00; //= PINA & 0X01;
+    unsigned char tmpB = 0x00; // = PINA & 0x02;
+    unsigned char tmpC = 0x00;
+    unsigned char tmpD = 0x00;
+    unsigned char totalWeight = 0x00;
+  //  unsigned char difference = 0x00;
 
     while (1) {
 	tmpA = PINA; // & 0x08;
 	tmpB = PINB; // & 0x04;
 	tmpC = PINC; // & 0x02;
-	tmpD = 0x00;
+//	difference = 0x00;
 	totalWeight = tmpA + tmpB + tmpC;
-	
+	tmpD = totalWeight >> 2;
+	tmpD = tmpD & 0xFC;
+
 	if (totalWeight > 140) {
 		tmpD = tmpD | 0x01; //0000001
 	}
 
-	if (tmpA > tmpC) {
-		difference = tmpA - tmpC; //00000010
-	}
-	else {
-		difference = tmpC - tmpA;
-	}
+//	if (tmpA > tmpC) {
+//		difference = tmpA - tmpC; //00000010
+//	}
+//	else {
+//		difference = tmpC - tmpA;
+//	}
 
 
-	if (difference > 80) {
+	if (abs(tmpA - tmpC) > 80) {
 		tmpD = tmpD | 0x02;
 	}
 	
-	totalWeight = totalWeight >> 2;
+//	totalWeight = totalWeight >> 2;
 
-	PORTD = tmpD | totalWeight;    
+	PORTD = tmpD;    
     }
     return 1;
 }
